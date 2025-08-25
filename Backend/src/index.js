6d894cc337f9571e9d7d3e4a0488998f43d71122
Kashish Gupta <kashishgupta1039@gmail.com>
2025-08-25 14:36:43 +0530

    resume builder

---
+++ Backend/src/index.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+import { connectDB } from "./db/index.js";
+import { config } from "dotenv";
+config();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on http://localhost:" + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("🔴 Failed to connect to MongoDB!", err);
+    process.exit(1);
+  });
